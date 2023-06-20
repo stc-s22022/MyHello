cb38859b84ef5c0be9212c46e2b3cb57f806264c
stc-s22022 <s22022@suntech.jp>
2023-06-20 10:57:19 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/s22022/myhello/MainActivity.java
@@ -0,0 +1,56 @@
+package jp.suntech.s22022.myhello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //ボタン・オブジェクトの用意
+        Button btClick = findViewById(R.id.btClick);
+        //リスナ・オブジェクトの用意
+        HelloListener listener = new HelloListener();
+        //ボタンにリスナを設定
+        btClick.setOnClickListener(listener);
+
+        //クリアボタンであるButtonオブジェクトを取得
+        Button btClear = findViewById(R.id.btClear);
+        //クリアボタンにリスナを設定
+        btClear.setOnClickListener(listener);
+    }
+
+    //ボタンをクリックした時のリスナクラス
+    private class HelloListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view){
+            //名前入力欄であるEditTextオブジェクトを取得
+            EditText input = findViewById(R.id.etName);
+            //メッセージを表示するTextViewオブジェクトを取得
+            TextView output = findViewById(R.id.tvOutput);
+
+            //タップされた画面部品のidのR値を取得
+            int id = view.getId();
+            //idのR値に応じて処理を分岐
+            if(id==R.id.btClick){
+                //入力された名前文字列を取得
+                String inputStr = input.getText().toString();
+                //メッセージを表示
+                output.setText(inputStr + "さん、こんにちは！");
+            }else if(id==R.id.btClear){
+                //名前入力欄を空文字に設定
+                input.setText("");
+                //メッセージ表示欄を空文字に設定
+                output.setText("");
+            }
+        }
+    }
+}
